6c6106e94f19b6012d1b9f0eb12c29a1196674bc
SmartWatt <smartwattana@gmail.com>
2023-10-04 16:54:12 +0700

    feat: student id only and compact

--- mysk-lib/src/models/student/db.rs
+++ mysk-lib/src/models/student/db.rs
@@ -41,17 +41,21 @@ impl BaseQuery for DbStudent {
 #[async_trait]
 impl GetById for DbStudent {
     async fn get_by_id(pool: &sqlx::PgPool, id: Uuid) -> Result<Self, sqlx::Error> {
-        sqlx::query_as!(DbStudent, r#"SELECT students.id, students.created_at, prefix_th, prefix_en, first_name_th, first_name_en, last_name_th, last_name_en, middle_name_th, middle_name_en, nickname_th, nickname_en, birthdate, citizen_id, profile, pants_size, shirt_size AS "shirt_size: _", blood_group AS "blood_group: _", sex AS "sex: _", student_id, user_id FROM students INNER JOIN people ON students.person_id = people.id WHERE students.id = $1"#, id) 
-            .fetch_one(pool)
-            .await
+        // sqlx::query_as!(DbStudent, r#"SELECT students.id, students.created_at, prefix_th, prefix_en, first_name_th, first_name_en, last_name_th, last_name_en, middle_name_th, middle_name_en, nickname_th, nickname_en, birthdate, citizen_id, profile, pants_size, shirt_size AS "shirt_size: _", blood_group AS "blood_group: _", sex AS "sex: _", student_id, user_id FROM students INNER JOIN people ON students.person_id = people.id WHERE students.id = $1"#, id)
+        //     .fetch_one(pool)
+        //     .await
+
+        sqlx::query_as::<_, DbStudent>(
+            format!("{} WHERE students.id = $1", Self::base_query()).as_str(),
+        )
+        .bind(id)
+        .fetch_one(pool)
+        .await
     }
 
-    async fn get_by_ids(
-        pool: &sqlx::PgPool,
-        ids: Vec<sqlx::types::Uuid>,
-    ) -> Result<Vec<Self>, sqlx::Error> {
+    async fn get_by_ids(pool: &sqlx::PgPool, ids: Vec<Uuid>) -> Result<Vec<Self>, sqlx::Error> {
         sqlx::query_as::<_, DbStudent>(
-            format!("{} WHERE id = ANY($1::uuid[])", Self::base_query()).as_str(),
+            format!("{} WHERE students.id = ANY($1)", Self::base_query()).as_str(),
         )
         .bind(ids)
         .fetch_all(pool)
