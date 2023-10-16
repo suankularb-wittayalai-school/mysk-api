c6f35c7eecc6db82b74bd01b2867ad999ba47740
Jimmy-Tempest <smartwattana@gmail.com>
2023-10-16 21:00:02 +0700

    feat: add default student without descendant tables

--- mysk-lib/src/models/student/mod.rs
+++ mysk-lib/src/models/student/mod.rs
@@ -3,10 +3,13 @@ use sqlx::pool;
 
 use self::{
     db::DbStudent,
-    fetch_levels::{compact::CompactStudent, id_only::IdOnlyStudent},
+    fetch_levels::{compact::CompactStudent, default::DefaultStudent, id_only::IdOnlyStudent},
 };
 
-use super::common::{requests::FetchLevel, traits::CombineFromTable};
+use super::common::{
+    requests::FetchLevel,
+    traits::{FetchLevelVariant, TopLevelFromTable},
+};
 
 pub mod db;
 pub mod fetch_levels;
@@ -14,11 +17,12 @@ pub mod fetch_levels;
 #[derive(Debug, Deserialize)]
 pub enum Student {
     IdOnly(IdOnlyStudent),
-    CompactStudent(Box<CompactStudent>),
+    Compact(Box<CompactStudent>),
+    Default(Box<DefaultStudent>),
 }
 
 #[async_trait::async_trait]
-impl CombineFromTable<DbStudent> for Student {
+impl TopLevelFromTable<DbStudent> for Student {
     async fn combine_from_table(
         pool: &pool::Pool<sqlx::Postgres>,
         table: DbStudent,
@@ -27,12 +31,17 @@ impl CombineFromTable<DbStudent> for Student {
     ) -> Result<Self, sqlx::Error> {
         match fetch_level {
             Some(FetchLevel::IdOnly) => Ok(Self::IdOnly(table.into())),
-            Some(FetchLevel::Compact) => {
-                Ok(Self::CompactStudent(Box::new(CompactStudent::from(table))))
-            }
+            Some(FetchLevel::Compact) => Ok(Self::Compact(Box::new(CompactStudent::from(table)))),
+            Some(FetchLevel::Default) => Ok(Self::Default(Box::new(
+                DefaultStudent::from_table(pool, table, descendant_fetch_level).await?,
+            ))),
             // TODO
-            Some(_) => Ok(Self::CompactStudent(Box::new(CompactStudent::from(table)))),
-            None => Ok(Self::CompactStudent(Box::new(CompactStudent::from(table)))),
+            Some(_) => Ok(Self::Default(Box::new(
+                DefaultStudent::from_table(pool, table, descendant_fetch_level).await?,
+            ))),
+            None => Ok(Self::Default(Box::new(
+                DefaultStudent::from_table(pool, table, descendant_fetch_level).await?,
+            ))),
         }
     }
 }
