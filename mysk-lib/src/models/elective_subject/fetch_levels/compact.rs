26bfe5eb88ac63ef4db81e1fc49f9aa5e06fa373
SmartWatt <smartwattana@gmail.com>
2024-04-07 14:25:13 +0700

    feat: id-only and compact varient of elective subject

---
+++ mysk-lib/src/models/elective_subject/fetch_levels/compact.rs
@@ -0,0 +1,38 @@
+use crate::prelude::*;
+
+use serde::{Deserialize, Serialize};
+use sqlx::PgPool;
+use uuid::Uuid;
+
+use crate::models::{
+    common::{requests::FetchLevel, string::MultiLangString, traits::FetchLevelVariant},
+    elective_subject::db::DbElectiveSubject,
+};
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct CompactElectiveSubject {
+    pub id: Uuid,
+    pub name: MultiLangString,
+    pub code: MultiLangString,
+    pub short_name: MultiLangString,
+    pub class_size: i64,
+    pub cap_size: i64,
+}
+
+impl From<DbElectiveSubject> for CompactElectiveSubject {
+    fn from(subject: DbElectiveSubject) -> Self {
+        Self {
+            id: subject.id,
+            name: MultiLangString::new(subject.name_th, Some(subject.name_en)),
+            code: MultiLangString::new(subject.code_th, Some(subject.code_en)),
+            short_name: MultiLangString::new(
+                subject.short_name_th.unwrap_or("".to_string()),
+                subject.short_name_en,
+            ),
+            class_size: subject.class_size,
+            cap_size: subject.cap_size,
+        }
+    }
+}
+
+impl_fetch_level_variant_from!(CompactElectiveSubject, DbElectiveSubject);
