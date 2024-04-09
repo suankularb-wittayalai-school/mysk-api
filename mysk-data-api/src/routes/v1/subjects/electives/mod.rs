69391da4fc66c0504cf51ce1203024932b5cce6a
Phawat Suksiriwan <phawat@suksiriwan.com>
2024-04-09 21:43:31 +0700

    feature: query elective subject details

--- mysk-data-api/src/routes/v1/subjects/electives/mod.rs
+++ mysk-data-api/src/routes/v1/subjects/electives/mod.rs
@@ -1,7 +1,9 @@
 use actix_web::web;
 
+pub mod query_elective_details;
 pub mod query_electives;
 
 pub fn config(cfg: &mut web::ServiceConfig) {
     cfg.service(query_electives::query_elective_subject);
+    cfg.service(query_elective_details::query_elective_details);
 }
---
+++ mysk-data-api/src/routes/v1/subjects/electives/query_elective_details.rs
@@ -0,0 +1,38 @@
+use actix_web::{get, web, HttpResponse, Responder};
+
+use mysk_lib::{
+    models::{
+        common::{
+            requests::{QueryablePlaceholder, RequestType, SortablePlaceholder},
+            response::{MetadataType, ResponseType},
+            traits::TopLevelGetById,
+        },
+        elective_subject::ElectiveSubject,
+    },
+    prelude::*,
+};
+use uuid::Uuid;
+
+use crate::middlewares::api_key::HaveApiKey;
+use crate::AppState;
+
+#[get("/{id}")]
+pub async fn query_elective_details(
+    data: web::Data<AppState>,
+    path: web::Path<(Uuid,)>,
+    request_query: RequestType<ElectiveSubject, QueryablePlaceholder, SortablePlaceholder>,
+    _: HaveApiKey,
+) -> Result<impl Responder> {
+    let pool = &data.db;
+
+    let id = path.into_inner().0;
+    let fetch_level = request_query.fetch_level.as_ref();
+    let descendant_fetch_level = request_query.descendant_fetch_level.as_ref();
+
+    let elective_subject =
+        ElectiveSubject::get_by_id(pool, id, fetch_level, descendant_fetch_level).await?;
+
+    let response = ResponseType::new(elective_subject, Some(MetadataType::default()));
+
+    Ok(HttpResponse::Ok().json(response))
+}
