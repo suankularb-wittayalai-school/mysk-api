6fd68180f8649be70d66bb93c29e19efa7f87bad
owo93 <Iztyle30@gmail.com>
2024-04-16 20:27:29 +0700

    feat: query_offers

--- mysk-data-api/src/routes/v1/subjects/electives/trade_offers/query_offers.rs
+++ mysk-data-api/src/routes/v1/subjects/electives/trade_offers/query_offers.rs
@@ -1,34 +1,52 @@
-use crate::{
-    extractors::{api_key::ApiKeyHeader, student::LoggedInStudent},
-    AppState,
-};
-use actix_web::{
-    get,
-    web::{Data, Json},
-    HttpResponse, Responder,
-};
+use crate::{extractors::api_key::ApiKeyHeader, AppState};
+use actix_web::{get, web::Data, HttpResponse, Responder};
 use mysk_lib::{
     common::{
-        requests::{QueryablePlaceholder, RequestType, SortablePlaceholder},
-        response::ResponseType,
+        requests::RequestType,
+        response::{MetadataType, ResponseType},
     },
-    models::elective_trade_offer::ElectiveTradeOffer,
+    models::{
+        elective_trade_offer::{
+            request::{
+                queryable::QueryableElectiveTradeOffer, sortable::SortableElectiveTradeOffer,
+            },
+            ElectiveTradeOffer,
+        },
+        traits::TopLevelQuery,
+    },
     prelude::*,
 };
 
 #[get("")]
 pub async fn query_trade_offers(
     data: Data<AppState>,
-    request_body: Json<RequestType<ElectiveTradeOffer, QueryablePlaceholder, SortablePlaceholder>>,
-    student_id: LoggedInStudent,
+    request_body: RequestType<
+        ElectiveTradeOffer,
+        QueryableElectiveTradeOffer,
+        SortableElectiveTradeOffer,
+    >,
     _: ApiKeyHeader,
 ) -> Result<impl Responder> {
     let pool = &data.db;
-    // let fetch_level = request_body.fetch_level.as_ref();
-    // let descendant_fetch_level = request_body.descendant_fetch_level.as_ref();
-    // let filter = request_body.filter.as_ref();
-    // let sort = request_body.sort.as_ref();
-    // let pagination = request_body.pagination.as_ref();
+    let fetch_level = request_body.fetch_level.as_ref();
+    let descendant_fetch_level = request_body.descendant_fetch_level.as_ref();
+    let filter = request_body.filter.as_ref();
+    let sort = request_body.sort.as_ref();
+    let pagination = request_body.pagination.as_ref();
 
-    Ok(HttpResponse::Ok())
+    let offers = ElectiveTradeOffer::query(
+        pool,
+        fetch_level,
+        descendant_fetch_level,
+        filter,
+        sort,
+        pagination,
+    )
+    .await?;
+
+    let pagniation = ElectiveTradeOffer::response_pagination(pool, filter, pagination).await?;
+
+    let response = ResponseType::new(offers, Some(MetadataType::new(Some(pagniation))));
+
+    Ok(HttpResponse::Ok().json(response))
 }
