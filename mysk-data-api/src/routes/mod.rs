ec6f30384542c28f611b7997a0133b893f0ecd09
Phawat Suksiriwan <phawat@suksiriwan.com>
2024-04-19 19:44:20 +0700

    feat: global not found handler

--- mysk-data-api/src/routes/mod.rs
+++ mysk-data-api/src/routes/mod.rs
@@ -1,11 +1,13 @@
-use actix_web::web::{scope, ServiceConfig};
+use actix_web::web::{scope, to, ServiceConfig};
 
 pub mod auth;
 pub mod health;
+pub mod not_found;
 pub mod v1;
 
 pub fn config(cfg: &mut ServiceConfig) {
     cfg.service(scope("/auth").configure(auth::config));
     cfg.service(health::health_check);
     cfg.service(scope("/v1").configure(v1::config));
+    cfg.default_service(to(not_found::not_found));
 }
