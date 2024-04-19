e34d87fc3bd79916dc064e61314d94dda6bab696
Phawat Suksiriwan <phawat@suksiriwan.com>
2024-04-19 19:43:45 +0700

    fix: config update and version bump

--- mysk-lib/src/common/config.rs
+++ mysk-lib/src/common/config.rs
@@ -3,12 +3,12 @@ use std::net::IpAddr;
 
 #[derive(Debug, Clone)]
 pub struct Config {
-    pub client_origin: String,
     pub database_url: String,
     pub google_oauth_client_id: String,
     pub google_oauth_client_secret: String,
     pub host: IpAddr,
     pub port: u16,
+    pub root_uri: String,
     pub token_expired_in: String,
     pub token_max_age: u64,
     pub token_secret: String,
@@ -16,7 +16,6 @@ pub struct Config {
 
 impl Config {
     pub fn init() -> Config {
-        let client_origin = var("CLIENT_ORIGIN").expect("CLIENT_ORIGIN must be set");
         let database_url = var("DATABASE_URL").expect("DATABASE_URL must be set");
         let google_oauth_client_id =
             var("GOOGLE_OAUTH_CLIENT_ID").expect("GOOGLE_OAUTH_CLIENT_ID must be set");
@@ -30,6 +29,7 @@ impl Config {
             .unwrap_or("8000".into())
             .parse::<u16>()
             .expect("PORT must be a valid port number");
+        let root_uri = var("ROOT_URI").expect("ROOT_URI must be set");
         let token_expired_in = var("TOKEN_EXPIRED_IN").expect("TOKEN_EXPIRED_IN must be set");
         let token_max_age = var("TOKEN_MAXAGE")
             .expect("TOKEN_MAXAGE must be set")
@@ -38,12 +38,12 @@ impl Config {
         let token_secret = var("TOKEN_SECRET").expect("TOKEN_SECRET must be set");
 
         Config {
-            client_origin,
             database_url,
             google_oauth_client_id,
             google_oauth_client_secret,
             host,
             port,
+            root_uri,
             token_expired_in,
             token_max_age,
             token_secret,
