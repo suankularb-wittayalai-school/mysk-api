0a0b58bb2c6b60a5233f255a4220ae86f7e5d543
owo93 <Iztyle30@gmail.com>
2024-06-09 22:04:12 +0700

    feat: accepting and declining club_requests

--- mysk-data-api/src/routes/v1/clubs/requests/mod.rs
+++ mysk-data-api/src/routes/v1/clubs/requests/mod.rs
@@ -3,9 +3,11 @@ use actix_web::web::ServiceConfig;
 pub mod delete_club_requests;
 pub mod query_club_request_details;
 pub mod query_club_requests;
+pub mod update_club_requests;
 
 pub fn config(cfg: &mut ServiceConfig) {
     cfg.service(delete_club_requests::delete_club_requests);
     cfg.service(query_club_request_details::query_club_request_details);
     cfg.service(query_club_requests::query_club_requests);
+    cfg.service(update_club_requests::update_club_requests);
 }
---
+++ mysk-data-api/src/routes/v1/clubs/requests/update_club_requests.rs
@@ -0,0 +1,117 @@
+use crate::{
+    extractors::{api_key::ApiKeyHeader, student::LoggedInStudent},
+    AppState,
+};
+use actix_web::{
+    put,
+    web::{Data, Json, Path},
+    HttpResponse, Responder,
+};
+use log::info;
+use mysk_lib::{
+    common::{
+        requests::{FetchLevel, QueryablePlaceholder, RequestType, SortablePlaceholder},
+        response::{self, ResponseType},
+    },
+    models::{
+        club::{request, Club},
+        club_request::{db::DbClubRequest, ClubRequest},
+        enums::SubmissionStatus,
+        student::Student,
+        traits::TopLevelGetById as _,
+    },
+    prelude::*,
+};
+use serde::Deserialize;
+use sqlx::{query, query_as};
+use uuid::Uuid;
+
+#[derive(Debug, Deserialize)]
+struct UpdatableClubRequest {
+    pub status: SubmissionStatus,
+}
+
+#[put("/{id}")]
+pub async fn update_club_requests(
+    data: Data<AppState>,
+    club_request_id: Path<Uuid>,
+    request_body: Json<
+        RequestType<UpdatableClubRequest, QueryablePlaceholder, SortablePlaceholder>,
+    >,
+    student_id: LoggedInStudent,
+    _: ApiKeyHeader,
+) -> Result<impl Responder> {
+    let pool = &data.db;
+    let client_student_id = student_id.0;
+    let club_request_id = club_request_id.into_inner();
+    let club_request_status = match &request_body.data {
+        Some(request_data) => match request_data.status {
+            SubmissionStatus::Approved | SubmissionStatus::Declined => request_data.status,
+            SubmissionStatus::Pending => {
+                return Err(Error::InvalidRequest(
+                    "Status must be either 'approved' or 'declined'".to_string(),
+                    format!("/clubs/requests/{club_request_id}"),
+                ));
+            }
+        },
+        _ => unreachable!("JSON errors are pre-handled by the JsonConfig error handler"),
+    };
+    let fetch_level = request_body.fetch_level.as_ref();
+    let descendant_fetch_level = request_body.descendant_fetch_level.as_ref();
+
+    // Check if the club request exists
+    let club_request = match ClubRequest::get_by_id(
+        pool,
+        club_request_id,
+        Some(&FetchLevel::Default),
+        Some(&FetchLevel::IdOnly),
+    )
+    .await
+    {
+        Ok(ClubRequest::Default(club_request, _)) => club_request,
+        Err(Error::InternalSeverError(_, _)) => {
+            return Err(Error::EntityNotFound(
+                "Club request not found".to_string(),
+                format!("/clubs/requests/{club_request_id}"),
+            ))
+        }
+        _ => unreachable!("ClubRequest::get_by_id should always return a Default variant"),
+    };
+
+    // Check if the club request is still pending
+    match club_request.membership_status {
+        SubmissionStatus::Approved | SubmissionStatus::Declined => {
+            return Err(Error::InvalidPermission(
+                format!(
+                    "Club request has already been {}",
+                    club_request.membership_status
+                ),
+                format!("/clubs/requests/{club_request_id}"),
+            ))
+        }
+        SubmissionStatus::Pending => (),
+    }
+
+    let mut updated_status: Option<SubmissionStatus> = club_request_status.into();
+    dbg!(club_request_status);
+    // Update the club request status based on the approval or decline
+    query!(
+        r#"UPDATE club_members SET membership_status = $1 WHERE id = $2"#,
+        updated_status.unwrap() as SubmissionStatus,
+        club_request_id,
+    )
+    .execute(pool)
+    .await?;
+
+    let updated_club_request = ClubRequest::get_by_id(
+        pool,
+        club_request_id,
+        Some(&FetchLevel::Detailed),
+        Some(&FetchLevel::IdOnly),
+    )
+    .await?;
+
+    let response = ResponseType::new(updated_club_request, None);
+
+    Ok(HttpResponse::Ok().json(response))
+}
