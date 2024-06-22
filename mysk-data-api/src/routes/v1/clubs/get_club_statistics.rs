4ee033219b770389b320b5723fa1c03c5cfd749b
owo93 <Iztyle30@gmail.com>
2024-06-22 14:37:43 +0700

    feat: query club statistics

---
+++ mysk-data-api/src/routes/v1/clubs/get_club_statistics.rs
@@ -0,0 +1,73 @@
+use crate::{extractors::api_key::ApiKeyHeader, AppState};
+use actix_web::{get, web::Data, HttpResponse, Responder};
+use mysk_lib::{
+    common::response::ResponseType, helpers::date::get_current_academic_year, prelude::*,
+};
+use serde::Serialize;
+use sqlx::{query, PgPool};
+
+#[derive(Debug, Serialize)]
+struct ClubStatistics {
+    pub new_club_members: i64,
+    pub new_club_staffs: i64,
+    pub active_clubs: i64,
+    pub total_students: i64,
+}
+
+impl ClubStatistics {
+    pub async fn new(pool: &PgPool) -> Result<Self> {
+        let current_year = get_current_academic_year(None);
+
+        let new_club_members = query!(
+            "SELECT COUNT(DISTINCT student_id) as count FROM club_members WHERE year = $1",
+            current_year
+        )
+        .fetch_one(pool)
+        .await?
+        .count
+        .unwrap_or(0);
+
+        let new_club_staffs = query!(
+            "SELECT COUNT(DISTINCT student_id) as count FROM club_staffs WHERE year = $1",
+            current_year
+        )
+        .fetch_one(pool)
+        .await?
+        .count
+        .unwrap_or(0);
+
+        let active_clubs = query!(
+            "SELECT COUNT(DISTINCT club_id) as count FROM club_staffs JOIN clubs ON club_staffs.club_id = clubs.id WHERE club_staffs.year = $1",
+            current_year
+        )
+        .fetch_one(pool)
+        .await?
+        .count
+        .unwrap_or(0);
+
+        // Total students for percentage calculation
+        let total_students = query!(
+            "SELECT count(DISTINCT students.id) as count FROM students JOIN classroom_students  ON classroom_students.student_id = students.id JOIN classrooms  ON classrooms.id = classroom_students.classroom_id AND classrooms.year = $1", current_year
+        )
+        .fetch_one(pool)
+        .await?
+        .count
+        .unwrap_or(0);
+
+        Ok(ClubStatistics {
+            new_club_members,
+            new_club_staffs,
+            active_clubs,
+            total_students,
+        })
+    }
+}
+
+#[get("/statistics")]
+async fn get_club_statistics(data: Data<AppState>) -> Result<impl Responder> {
+    let pool = &data.db;
+    let statistics = ClubStatistics::new(pool).await?;
+    let response = ResponseType::new(statistics, None);
+
+    Ok(HttpResponse::Ok().json(response))
+}
--- mysk-data-api/src/routes/v1/clubs/mod.rs
+++ mysk-data-api/src/routes/v1/clubs/mod.rs
@@ -2,6 +2,7 @@ use actix_web::web::{scope, ServiceConfig};
 
 pub mod add_club_members;
 pub mod create_club_contacts;
+pub mod get_club_statistics;
 pub mod join_clubs;
 pub mod query_club_details;
 pub mod query_clubs;
@@ -9,6 +10,7 @@ pub mod requests;
 
 pub fn config(cfg: &mut ServiceConfig) {
     cfg.service(scope("/requests").configure(requests::config));
+    cfg.service(get_club_statistics::get_club_statistics);
     cfg.service(add_club_members::add_club_members);
     cfg.service(join_clubs::join_clubs);
     cfg.service(create_club_contacts::create_club_contacts);
