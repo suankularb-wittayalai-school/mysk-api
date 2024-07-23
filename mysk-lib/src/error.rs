3f09c68d74a529a47fa1c536fcaa509c3214af48
Phawat Suksiriwan <phawat@suksiriwan.com>
2024-07-24 00:44:14 +0700

    refactor: better errors

--- mysk-lib/src/error.rs
+++ mysk-lib/src/error.rs
@@ -1,7 +1,12 @@
 use crate::common::response::{ErrorResponseType, ErrorType};
 use actix_web::{HttpResponse, ResponseError};
 use serde::Serialize;
-use std::fmt::{Display, Formatter};
+use sqlx::Error as SqlxError;
+use std::{
+    fmt::{Display, Formatter},
+    panic,
+};
+use tokio::task::JoinError;
 use uuid::Uuid;
 
 #[allow(clippy::doc_markdown)]
@@ -11,33 +16,152 @@ use uuid::Uuid;
 #[derive(Debug, Serialize)]
 #[serde(rename_all = "snake_case")]
 pub enum Error {
+    // Client Errors
     /// HTTP 400 - [Bad Request](https://developer.mozilla.org/docs/Web/HTTP/Status/400)
     InvalidRequest(String, String),
+
     /// HTTP 404 - [Not Found](https://developer.mozilla.org/docs/Web/HTTP/Status/404)
     EntityNotFound(String, String),
-    /// HTTP 403 - [Forbidden](https://developer.mozilla.org/docs/Web/HTTP/Status/403)
-    InvalidPermission(String, String),
+
     /// HTTP 409 - [Conflict](https://developer.mozilla.org/docs/Web/HTTP/Status/409)
     Conflicted(String, String),
+
+    // Authentication Errors
+    /// HTTP 401 - [Unauthorized](https://developer.mozilla.org/docs/Web/HTTP/Status/401)
+    MissingApiKey(String, String),
+
+    /// HTTP 401 - [Unauthorized](https://developer.mozilla.org/docs/Web/HTTP/Status/401)
+    InvalidApiKey(String, String),
+
+    /// HTTP 401 - [Unauthorized](https://developer.mozilla.org/docs/Web/HTTP/Status/401)
+    InvalidAuthorizationScheme(String, String),
+
+    /// HTTP 401 - [Unauthorized](https://developer.mozilla.org/docs/Web/HTTP/Status/401)
+    MissingToken(String, String),
+
+    /// HTTP 401 - [Unauthorized](https://developer.mozilla.org/docs/Web/HTTP/Status/401)
+    InvalidToken(String, String),
+
+    // Authorization Error
+    /// HTTP 403 - [Forbidden](https://developer.mozilla.org/docs/Web/HTTP/Status/403)
+    InvalidPermission(String, String),
+
+    // Server Errors
     /// HTTP 500 - [Internal Server Error](https://developer.mozilla.org/docs/Web/HTTP/Status/500)
     InternalSeverError(String, String),
 
-    // Auth errors
-    /// HTTP 401 - [Unauthorized](https://developer.mozilla.org/docs/Web/HTTP/Status/401)
-    InvalidAuthorizationScheme(String, String),
-    /// HTTP 401 - [Unauthorized](https://developer.mozilla.org/docs/Web/HTTP/Status/401)
-    InvalidToken(String, String),
-    /// HTTP 401 - [Unauthorized](https://developer.mozilla.org/docs/Web/HTTP/Status/401)
-    MissingToken(String, String),
-    /// HTTP 401 - [Unauthorized](https://developer.mozilla.org/docs/Web/HTTP/Status/401)
-    MissingApiKey(String, String),
-    /// HTTP 401 - [Unauthorized](https://developer.mozilla.org/docs/Web/HTTP/Status/401)
-    InvalidApiKey(String, String),
+    /// HTTP 503 -
+    /// [Service Unavailable](https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/503)
+    ServiceUnavailable(String, String),
+}
+
+impl Display for Error {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        let error = match self {
+            // Client Errors
+            Error::InvalidRequest(detail, source) => {
+                format!("Invalid request: {detail} (source: {source})")
+            }
+            Error::EntityNotFound(detail, source) => {
+                format!("Entity not found: {detail} (source: {source})")
+            }
+            Error::Conflicted(detail, source) => {
+                format!("Conflicted: {detail} (source: {source})")
+            }
+            // Authentication Errors
+            Error::MissingApiKey(detail, source) => {
+                format!("Missing API key: {detail} (source: {source})")
+            }
+            Error::InvalidApiKey(detail, source) => {
+                format!("Invalid API key: {detail} (source: {source})")
+            }
+            Error::InvalidAuthorizationScheme(detail, source) => {
+                format!("Invalid authorization scheme: {detail} (source: {source})")
+            }
+            Error::MissingToken(detail, source) => {
+                format!("Missing token: {detail} (source: {source})")
+            }
+            Error::InvalidToken(detail, source) => {
+                format!("Invalid token: {detail} (source: {source})")
+            }
+            // Authorization Error
+            Error::InvalidPermission(detail, source) => {
+                format!("Invalid permission: {detail} (source: {source})")
+            }
+            // Server Errors
+            Error::InternalSeverError(detail, source) => {
+                format!("Internal server error: {detail} (source: {source})")
+            }
+            Error::ServiceUnavailable(detail, source) => {
+                format!("Service unavailable: {detail} (source: {source})")
+            }
+        };
+
+        write!(f, "{error}")
+    }
+}
+
+impl From<JoinError> for Error {
+    fn from(value: JoinError) -> Self {
+        if value.is_panic() {
+            panic::resume_unwind(value.into_panic());
+        }
+
+        Error::InternalSeverError("Internal server error".to_string(), "Tokio".to_string())
+    }
+}
+
+impl From<SqlxError> for Error {
+    fn from(value: SqlxError) -> Self {
+        match value {
+            SqlxError::Database(source) => {
+                Error::InvalidRequest(source.message().to_string(), "SQLx".to_string())
+            }
+            SqlxError::RowNotFound => {
+                Error::EntityNotFound("Entity not found".to_string(), "SQLx".to_string())
+            }
+            SqlxError::Io(_)
+            | SqlxError::Tls(_)
+            | SqlxError::PoolTimedOut
+            | SqlxError::PoolClosed => {
+                Error::ServiceUnavailable("Service unavailable".to_string(), "SQLx".to_string())
+            }
+            #[cfg(debug_assertions)]
+            _ => Error::InternalSeverError(value.to_string(), "SQLx".to_string()),
+            #[cfg(not(debug_assertions))]
+            _ => Error::InternalSeverError("Internal server error".to_string(), "SQLx".to_string()),
+        }
+    }
+}
+
+impl From<&Error> for HttpResponse {
+    fn from(value: &Error) -> Self {
+        let response = ErrorResponseType::new(value.into(), None);
+
+        match value {
+            // Client Errors
+            Error::InvalidRequest(_, _) => HttpResponse::BadRequest().json(response),
+            Error::EntityNotFound(_, _) => HttpResponse::NotFound().json(response),
+            Error::Conflicted(_, _) => HttpResponse::Conflict().json(response),
+            // Authentication Errors
+            Error::MissingApiKey(_, _)
+            | Error::InvalidApiKey(_, _)
+            | Error::InvalidAuthorizationScheme(_, _)
+            | Error::MissingToken(_, _)
+            | Error::InvalidToken(_, _) => HttpResponse::Unauthorized().json(response),
+            // Authorization Error
+            Error::InvalidPermission(_, _) => HttpResponse::Forbidden().json(response),
+            // Server Errors
+            Error::InternalSeverError(_, _) => HttpResponse::InternalServerError().json(response),
+            Error::ServiceUnavailable(_, _) => HttpResponse::ServiceUnavailable().json(response),
+        }
+    }
 }
 
 impl From<&Error> for ErrorType {
-    fn from(val: &Error) -> Self {
-        match val {
+    fn from(value: &Error) -> Self {
+        match value {
+            // Client Errors
             Error::InvalidRequest(detail, source) => ErrorType {
                 id: Uuid::new_v4(),
                 code: 400,
@@ -52,13 +176,6 @@ impl From<&Error> for ErrorType {
                 detail: detail.to_string(),
                 source: source.to_string(),
             },
-            Error::InvalidPermission(detail, source) => ErrorType {
-                id: Uuid::new_v4(),
-                code: 403,
-                error_type: "invalid_permission".to_string(),
-                detail: detail.to_string(),
-                source: source.to_string(),
-            },
             Error::Conflicted(detail, source) => ErrorType {
                 id: Uuid::new_v4(),
                 code: 409,
@@ -66,34 +183,7 @@ impl From<&Error> for ErrorType {
                 detail: detail.to_string(),
                 source: source.to_string(),
             },
-            Error::InternalSeverError(detail, source) => ErrorType {
-                id: Uuid::new_v4(),
-                code: 500,
-                error_type: "internal_server_error".to_string(),
-                detail: detail.to_string(),
-                source: source.to_string(),
-            },
-            Error::InvalidAuthorizationScheme(detail, source) => ErrorType {
-                id: Uuid::new_v4(),
-                code: 401,
-                error_type: "invalid_authorization_scheme".to_string(),
-                detail: detail.to_string(),
-                source: source.to_string(),
-            },
-            Error::InvalidToken(detail, source) => ErrorType {
-                id: Uuid::new_v4(),
-                code: 401,
-                error_type: "invalid_token".to_string(),
-                detail: detail.to_string(),
-                source: source.to_string(),
-            },
-            Error::MissingToken(detail, source) => ErrorType {
-                id: Uuid::new_v4(),
-                code: 401,
-                error_type: "missing_token".to_string(),
-                detail: detail.to_string(),
-                source: source.to_string(),
-            },
+            // Authentication Errors
             Error::MissingApiKey(detail, source) => ErrorType {
                 id: Uuid::new_v4(),
                 code: 401,
@@ -108,99 +198,51 @@ impl From<&Error> for ErrorType {
                 detail: detail.to_string(),
                 source: source.to_string(),
             },
+            Error::InvalidAuthorizationScheme(detail, source) => ErrorType {
+                id: Uuid::new_v4(),
+                code: 401,
+                error_type: "invalid_authorization_scheme".to_string(),
+                detail: detail.to_string(),
+                source: source.to_string(),
+            },
+            Error::MissingToken(detail, source) => ErrorType {
+                id: Uuid::new_v4(),
+                code: 401,
+                error_type: "missing_token".to_string(),
+                detail: detail.to_string(),
+                source: source.to_string(),
+            },
+            Error::InvalidToken(detail, source) => ErrorType {
+                id: Uuid::new_v4(),
+                code: 401,
+                error_type: "invalid_token".to_string(),
+                detail: detail.to_string(),
+                source: source.to_string(),
+            },
+            // Authorization Error
+            Error::InvalidPermission(detail, source) => ErrorType {
+                id: Uuid::new_v4(),
+                code: 403,
+                error_type: "invalid_permission".to_string(),
+                detail: detail.to_string(),
+                source: source.to_string(),
+            },
+            // Server Errors
+            Error::InternalSeverError(detail, source) => ErrorType {
+                id: Uuid::new_v4(),
+                code: 500,
+                error_type: "internal_server_error".to_string(),
+                detail: detail.to_string(),
+                source: source.to_string(),
+            },
+            Error::ServiceUnavailable(detail, source) => ErrorType {
+                id: Uuid::new_v4(),
+                code: 503,
+                error_type: "service_unavailable".to_string(),
+                detail: detail.to_string(),
+                source: source.to_string(),
+            },
         }
-    }
-}
-
-impl From<sqlx::Error> for Error {
-    fn from(val: sqlx::Error) -> Self {
-        Error::InternalSeverError(val.to_string(), "sqlx".to_string())
-    }
-}
-
-impl From<&Error> for HttpResponse {
-    fn from(val: &Error) -> Self {
-        let res_val: ErrorResponseType = val.into();
-        match val {
-            Error::InvalidRequest(_, _) => HttpResponse::BadRequest().json(res_val),
-            Error::EntityNotFound(_, _) => HttpResponse::NotFound().json(res_val),
-            Error::InvalidPermission(_, _) => HttpResponse::Forbidden().json(res_val),
-            Error::Conflicted(_, _) => HttpResponse::Conflict().json(res_val),
-            Error::InternalSeverError(_, _) => HttpResponse::InternalServerError().json(res_val),
-            Error::InvalidAuthorizationScheme(_, _)
-            | Error::InvalidToken(_, _)
-            | Error::MissingToken(_, _)
-            | Error::MissingApiKey(_, _)
-            | Error::InvalidApiKey(_, _) => HttpResponse::Unauthorized().json(res_val),
-        }
-    }
-}
-
-impl AsRef<Error> for Error {
-    fn as_ref(&self) -> &Error {
-        self
-    }
-}
-
-impl From<Error> for HttpResponse {
-    fn from(val: Error) -> Self {
-        (&val).into()
-    }
-}
-
-impl From<Error> for ErrorType {
-    fn from(val: Error) -> Self {
-        (&val).into()
-    }
-}
-
-impl From<&Error> for ErrorResponseType {
-    fn from(val: &Error) -> Self {
-        ErrorResponseType::new(val.into(), None)
-    }
-}
-
-impl From<Error> for ErrorResponseType {
-    fn from(val: Error) -> Self {
-        (&val).into()
-    }
-}
-
-impl Display for Error {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        let error = match self {
-            Error::InvalidRequest(detail, source) => {
-                format!("Invalid request: {detail} (source: {source})")
-            }
-            Error::EntityNotFound(detail, source) => {
-                format!("Entity not found: {detail} (source: {source})")
-            }
-            Error::InvalidPermission(detail, source) => {
-                format!("Invalid permission: {detail} (source: {source})")
-            }
-            Error::Conflicted(detail, source) => {
-                format!("Conflicted: {detail} (source: {source})")
-            }
-            Error::InternalSeverError(detail, source) => {
-                format!("Internal server error: {detail} (source: {source})")
-            }
-            Error::InvalidAuthorizationScheme(detail, source) => {
-                format!("Invalid authorization scheme: {detail} (source: {source})")
-            }
-            Error::InvalidToken(detail, source) => {
-                format!("Invalid token: {detail} (source: {source})")
-            }
-            Error::MissingToken(detail, source) => {
-                format!("Missing token: {detail} (source: {source})")
-            }
-            Error::MissingApiKey(detail, source) => {
-                format!("Missing API key: {detail} (source: {source})")
-            }
-            Error::InvalidApiKey(detailed, source) => {
-                format!("Invalid API key: {detailed} (source: {source})")
-            }
-        };
-        write!(f, "{error}")
     }
 }
 
