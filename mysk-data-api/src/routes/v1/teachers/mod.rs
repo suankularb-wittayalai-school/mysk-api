2e3b13bc867ddb2a4ae418f1393f7c3e9cdc1954
owo93 <Iztyle30@gmail.com>
2024-07-27 15:41:18 +0700

    feat(teachers): query_teachers

--- mysk-data-api/src/routes/v1/teachers/mod.rs
+++ mysk-data-api/src/routes/v1/teachers/mod.rs
@@ -1,7 +1,9 @@
 use actix_web::web::ServiceConfig;
 
 pub mod get_teacher_by_id;
+pub mod query_teachers;
 
 pub fn config(cfg: &mut ServiceConfig) {
     cfg.service(get_teacher_by_id::get_teacher_by_id);
+    cfg.service(query_teachers::query_teachers);
 }
