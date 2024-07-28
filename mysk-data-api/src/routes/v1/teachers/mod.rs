bb96d5418007a01d23265fff81b59808ac4fc421
owo93 <Iztyle30@gmail.com>
2024-07-28 16:05:43 +0700

    feat(teachers): Create teacher contacts

--- mysk-data-api/src/routes/v1/teachers/mod.rs
+++ mysk-data-api/src/routes/v1/teachers/mod.rs
@@ -1,9 +1,11 @@
 use actix_web::web::ServiceConfig;
 
+pub mod create_teacher_contacts;
 pub mod get_teacher_by_id;
 pub mod query_teachers;
 
 pub fn config(cfg: &mut ServiceConfig) {
     cfg.service(get_teacher_by_id::get_teacher_by_id);
     cfg.service(query_teachers::query_teachers);
+    cfg.service(create_teacher_contacts::create_teacher_contacts);
 }
