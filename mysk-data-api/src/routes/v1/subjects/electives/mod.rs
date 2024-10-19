855057f75939137fcc431eb5d3ba10238b31fba4
Phawat Suksiriwan <phawat@suksiriwan.com>
2024-10-19 20:01:01 +0700

    feat: get previously enrolled elective

--- mysk-data-api/src/routes/v1/subjects/electives/mod.rs
+++ mysk-data-api/src/routes/v1/subjects/electives/mod.rs
@@ -1,6 +1,7 @@
 use actix_web::web::{scope, ServiceConfig};
 
 pub mod enroll_electives;
+pub mod get_previously_enrolled;
 pub mod in_enrollment_period;
 pub mod modify_electives;
 pub mod query_elective_details;
@@ -9,6 +10,7 @@ pub mod trade_offers;
 
 pub fn config(cfg: &mut ServiceConfig) {
     cfg.service(scope("/trade-offers").configure(trade_offers::config));
+    cfg.service(get_previously_enrolled::get_previously_enrolled);
     cfg.service(in_enrollment_period::in_enrollment_period);
     cfg.service(enroll_electives::enroll_elective_subject);
     cfg.service(modify_electives::modify_elective_subject);
