1731d1f63121f0f0fd6d64d59082c00148e9b621
Phawat Suksiriwan <phawat@suksiriwan.com>
2024-10-22 16:20:40 +0700

    fix: year and semester checking of electives

--- mysk-lib/src/models/elective_subject/db.rs
+++ mysk-lib/src/models/elective_subject/db.rs
@@ -44,6 +44,7 @@ pub struct DbElectiveSubject {
     pub credit: f64,
     pub description_th: Option<String>,
     pub description_en: Option<String>,
+    pub year: Option<i64>,
     pub semester: Option<i64>,
     pub subject_group_id: i64,
     pub syllabus: Option<String>,
