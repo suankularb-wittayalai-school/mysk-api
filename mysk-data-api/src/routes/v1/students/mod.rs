fa423007bde4d50620a187723c8f837186d14219
Phawat Suksiriwan <phawat@suksiriwan.com>
2025-01-14 23:06:25 +0700

    feat: modify students

--- mysk-data-api/src/routes/v1/students/mod.rs
+++ mysk-data-api/src/routes/v1/students/mod.rs
@@ -1,11 +1,13 @@
 use actix_web::web::ServiceConfig;
 
 pub mod create_student_contacts;
+pub mod modify_student;
 pub mod query_student_details;
 pub mod query_students;
 
 pub fn config(cfg: &mut ServiceConfig) {
     cfg.service(create_student_contacts::create_student_contacts);
+    cfg.service(modify_student::modify_student);
     cfg.service(query_student_details::query_student_details);
     cfg.service(query_students::query_students);
 }
