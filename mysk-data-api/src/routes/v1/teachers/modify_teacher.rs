2611476ebad5e585565ee1330c5fda89aacdb2a3
Phawat Suksiriwan <phawat@suksiriwan.com>
2025-01-14 22:18:55 +0700

    feat: modify teachers (final)
    Co-authored-by: owo93 <Iztyle30@gmail.com>

--- mysk-data-api/src/routes/v1/teachers/modify_teacher.rs
+++ mysk-data-api/src/routes/v1/teachers/modify_teacher.rs
@@ -10,7 +10,7 @@ use actix_web::{
 use chrono::NaiveDate;
 use mysk_lib::{
     common::{
-        requests::{QueryablePlaceholder, RequestType, SortablePlaceholder},
+        requests::{QueryParam, QueryablePlaceholder, RequestType, SortablePlaceholder},
         response::ResponseType,
         string::FlexibleMultiLangString,
     },
@@ -22,10 +22,11 @@ use mysk_lib::{
     },
     permissions::{self, ActionType},
     prelude::*,
+    query::set_clause::SqlSetClause,
 };
 use mysk_lib_macros::traits::db::GetById as _;
 use serde::Deserialize;
-use sqlx::{query, Postgres, QueryBuilder};
+use sqlx::query;
 use uuid::Uuid;
 
 #[derive(Debug, Deserialize)]
@@ -59,14 +60,14 @@ pub async fn modify_teacher(
     _: ApiKeyHeader,
     user: LoggedIn,
     teacher_id: Path<Uuid>,
-    request_body: Json<
+    Json(request_body): Json<
         RequestType<UpdateTeacherRequest, QueryablePlaceholder, SortablePlaceholder>,
     >,
 ) -> Result<impl Responder> {
     let pool = &data.db;
     let user = user.0;
     let teacher_id = teacher_id.into_inner();
-    let Some(update_data) = &request_body.data else {
+    let Some(update_data) = request_body.data else {
         return Err(Error::InvalidRequest(
             "Json deserialize error: field `data` can not be empty".to_string(),
             format!("/teachers/{teacher_id}"),
@@ -87,26 +88,26 @@ pub async fn modify_teacher(
         .await?;
 
     // NOTE: Teacher-related updates
-    if let Some(teacher_update) = &update_data.teacher {
+    if let Some(tu) = update_data.teacher {
         let mut teacher_transaction = pool.begin().await?;
         let current_academic_year = get_current_academic_year(None);
 
         // Update subject group
-        if let Some(subject_group) = &teacher_update.subject_group_id {
-            let new_subject_group =
-                query!("SELECT id FROM subject_groups WHERE id = $1", subject_group)
-                    .fetch_one(pool)
-                    .await?;
+        if let Some(subject_group_id) = tu.subject_group_id {
+            let new_subject_group = query!(
+                "SELECT id FROM subject_groups WHERE id = $1",
+                subject_group_id
+            )
+            .fetch_one(pool)
+            .await?;
 
             let current_subject_group = db_teacher.subject_group_id;
 
             if current_subject_group != new_subject_group.id {
                 query!(
-                    "UPDATE teachers
-                    SET subject_group_id = $1
-                    WHERE id = $2",
+                    "UPDATE teachers SET subject_group_id = $1 WHERE id = $2",
                     new_subject_group.id,
-                    teacher_id
+                    teacher_id,
                 )
                 .execute(&mut *teacher_transaction)
                 .await?;
@@ -114,11 +115,11 @@ pub async fn modify_teacher(
         };
 
         // Update/insert advisory classroom
-        if let Some(class_advisor_at) = &teacher_update.advisor_at {
+        if let Some(class_advisor_at) = tu.advisor_at {
             let new_classroom = query!(
                 "SELECT id FROM classrooms WHERE number = $1 AND year = $2 FOR UPDATE",
                 class_advisor_at,
-                current_academic_year
+                current_academic_year,
             )
             .fetch_one(pool)
             .await?;
@@ -132,12 +133,13 @@ pub async fn modify_teacher(
                     Some(existing_classroom) => {
                         // Change advisory classroom to new classroom
                         query!(
-                            "UPDATE classroom_advisors
-                            SET classroom_id = $1
-                            WHERE teacher_id = $2 AND classroom_id = $3",
+                            "
+                            UPDATE classroom_advisors SET classroom_id = $1
+                            WHERE teacher_id = $2 AND classroom_id = $3
+                            ",
                             new_classroom.id,
                             teacher_id,
-                            existing_classroom
+                            existing_classroom,
                         )
                         .execute(&mut *teacher_transaction)
                         .await?;
@@ -145,10 +147,12 @@ pub async fn modify_teacher(
                     None => {
                         // If the teacher isn't an advisor, add them to a classroom
                         query!(
-                            "INSERT INTO classroom_advisors (classroom_id, teacher_id) 
-                            VALUES ($1, $2)",
+                            "
+                            INSERT INTO classroom_advisors (classroom_id, teacher_id)
+                            VALUES ($1, $2)
+                            ",
                             new_classroom.id,
-                            teacher_id
+                            teacher_id,
                         )
                         .execute(&mut *teacher_transaction)
                         .await?;
@@ -160,95 +164,48 @@ pub async fn modify_teacher(
     }
 
     // NOTE: Person-related updates
-    if let Some(person_update) = &update_data.person {
+    if let Some(pu) = update_data.person {
         let mut person_transaction = pool.begin().await?;
-        let mut updates = Vec::new();
-        let mut bindings = Vec::new();
-        let mut coalesce_flags = Vec::new();
 
-        // Update allergies on a separate table `person_allergies`, remember to remove debug traces!!!
-        if let Some(allergies) = &person_update.allergies {
+        // Update allergies on a separate table `person_allergies`
+        if let Some(allergies) = pu.allergies {
             query!(
                 "DELETE FROM person_allergies WHERE person_id = $1",
-                person_id
+                person_id,
             )
             .execute(&mut *person_transaction)
             .await?;
 
-            for allergy in allergies {
-                query!(
-                    "INSERT INTO person_allergies (person_id, allergy_name) VALUES ($1, $2)",
-                    person_id,
-                    allergy
-                )
-                .execute(&mut *person_transaction)
-                .await?;
-            }
+            // See also: https://github.com/launchbadge/sqlx/blob/main/FAQ.md#how-can-i-bind-an-array-to-a-values-clause-how-can-i-do-bulk-inserts
+            query!(
+                "
+                INSERT INTO person_allergies (person_id, allergy_name)
+                SELECT $1, * FROM UNNEST($2::text[])
+                ",
+                person_id,
+                &allergies[..],
+            )
+            .execute(&mut *person_transaction)
+            .await?;
         };
 
-        macro_rules! add_update_field {
-            // NOTE: This handles `Option<FlexibleMultiLangString>`
-            (multilang: $field:ident, $value:expr) => {
-                if let Some(new_value) = $value {
-                    if let Some(th) = &new_value.th {
-                        updates.push(stringify!($field).to_owned() + "_th");
-                        bindings.push(th.clone());
-                        coalesce_flags.push(true);
-                    }
+        // TODO: Refactor `SqlSetClause` API
+        let mut qb = SqlSetClause::new()
+            .push_update_field_no_sep("birthdate", pu.birthdate, |p| QueryParam::NaiveDate(p))
+            .push_update_field("shirt_size", pu.shirt_size, |p| QueryParam::ShirtSize(p))
+            .push_update_field("pants_size", pu.pants_size, |p| QueryParam::String(p))
+            .push_multilang_update_field("prefix", pu.prefix)
+            .push_multilang_update_field("first_name", pu.first_name)
+            .push_multilang_update_field("last_name", pu.last_name)
+            .push_multilang_update_field("middle_name", pu.middle_name)
+            .push_multilang_update_field("nickname", pu.nickname)
+            .into_query_builder("UPDATE people");
 
-                    if let Some(en) = &new_value.en {
-                        updates.push(stringify!($field).to_owned() + "_en");
-                        bindings.push(en.clone());
-                        coalesce_flags.push(true);
-                    }
-                };
-            };
-
-            ($field:ident, $value:expr) => {
-                if let Some(new_value) = $value {
-                    updates.push(stringify!($field).to_owned());
-                    bindings.push(new_value.to_string()); // FIXME: This casts everything to a string
-                    coalesce_flags.push(false);
-                }
-            };
-        }
-
-        add_update_field!(multilang: prefix, &person_update.prefix);
-        add_update_field!(multilang: first_name, &person_update.first_name);
-        add_update_field!(multilang: last_name, &person_update.last_name);
-        add_update_field!(multilang: middle_name, &person_update.middle_name );
-        add_update_field!(multilang: nickname, &person_update.nickname);
-        add_update_field!(birthdate, &person_update.birthdate);
-        add_update_field!(shirt_size, &person_update.shirt_size);
-        add_update_field!(pants_size, &person_update.pants_size);
-
-        if !updates.is_empty() {
-            let mut query = QueryBuilder::new("UPDATE people SET ");
-            for (i, (field, use_coalesce)) in updates.iter().zip(coalesce_flags.iter()).enumerate()
-            {
-                if i > 0 {
-                    query.push(", ");
-                }
-
-                if *use_coalesce {
-                    query.push(format!("{field} = COALESCE("));
-                    query.push_bind(&bindings[i]);
-                    query.push(", ");
-                    query.push(format!("{field})"));
-                } else {
-                    query.push(format!("{field} = "));
-                    query.push_bind(&bindings[i]);
-                }
-            }
-
-            query.push(" WHERE id = ");
-            query.push_bind(person_id);
-
-            query.push(" RETURNING id");
-
-            // println!("{}", query.into_sql());
-            query.build().execute(&mut *person_transaction).await?;
-        }
+        qb.push(" WHERE id = ")
+            .push_bind(person_id)
+            .build()
+            .execute(&mut *person_transaction)
+            .await?;
 
         person_transaction.commit().await?;
     };
@@ -261,7 +218,6 @@ pub async fn modify_teacher(
         &authorizer,
     )
     .await?;
-
     let response = ResponseType::new(teacher, None);
 
     Ok(HttpResponse::Ok().json(response))
