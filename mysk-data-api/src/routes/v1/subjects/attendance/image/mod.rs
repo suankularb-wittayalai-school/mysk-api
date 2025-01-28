8dcc97883d8ccafabb6d132c60813d72e92e2b78
Phawat Suksiriwan <phawat@suksiriwan.com>
2025-01-29 01:11:22 +0700

    fix: image upload payload size increase

--- mysk-data-api/src/routes/v1/subjects/attendance/image/mod.rs
+++ mysk-data-api/src/routes/v1/subjects/attendance/image/mod.rs
@@ -1,9 +1,11 @@
-use actix_web::web::ServiceConfig;
+use actix_web::web::{PayloadConfig, ServiceConfig};
 
 pub mod get_report_image;
 pub mod upload_report_image;
 
 pub fn config(cfg: &mut ServiceConfig) {
     cfg.service(get_report_image::get_report_image);
-    cfg.service(upload_report_image::upload_report_image);
+    // 4.5 * 2**20 = ~4.5 MiB (Vercel serverless functions limit)
+    cfg.app_data(PayloadConfig::new(4_718_592))
+        .service(upload_report_image::upload_report_image);
 }
