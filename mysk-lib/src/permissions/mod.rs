f430cd00c4a030cabb7dd869ca6125798ea6e3ed
SKISo <132156825+SK-IT-Solutions@users.noreply.github.com>
2025-02-03 20:13:55 +0700

    fix: temp

--- mysk-lib/src/permissions/mod.rs
+++ mysk-lib/src/permissions/mod.rs
@@ -1,5 +1,5 @@
-mod authorizer;
-mod roles;
+pub mod authorizer;
+pub mod roles;
 
 pub use authorizer::{
     authorize_default_read_only, authorize_read_only, deny, get_authorizer, ActionType, Authorizer,
