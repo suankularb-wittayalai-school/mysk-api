953cc655a365fc1e87917df020420de35c16fc29
owo93 <iztyle30@gmail.com>
2025-07-27 14:27:14 +0700

    feat: query_cheer_practice_attendance

--- mysk-data-api/src/routes/v1/students/mod.rs
+++ mysk-data-api/src/routes/v1/students/mod.rs
@@ -2,6 +2,7 @@ use actix_web::web::ServiceConfig;
 
 pub mod create_student_contacts;
 pub mod modify_student;
+pub mod query_cheer_practice_attendance;
 pub mod query_student_details;
 pub mod query_students;
 
@@ -9,5 +10,6 @@ pub fn config(cfg: &mut ServiceConfig) {
     cfg.service(create_student_contacts::create_student_contacts)
         .service(modify_student::modify_student)
         .service(query_student_details::query_student_details)
-        .service(query_students::query_students);
+        .service(query_students::query_students)
+        .service(query_cheer_practice_attendance::query_cheer_practice_attendance);
 }
