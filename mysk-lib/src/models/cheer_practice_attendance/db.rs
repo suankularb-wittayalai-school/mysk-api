82bda3ff9a4cac265fd1d7dceaa237d464a1402d
owo93 <iztyle30@gmail.com>
2025-07-27 14:26:28 +0700

    feat: get CheerPracticeAttendance record by student_id

--- mysk-lib/src/models/cheer_practice_attendance/db.rs
+++ mysk-lib/src/models/cheer_practice_attendance/db.rs
@@ -1,12 +1,13 @@
 use chrono::{DateTime, Utc};
 use mysk_lib_macros::GetById;
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 use sqlx::prelude::FromRow;
+use sqlx::{PgConnection, query};
 use uuid::Uuid;
 
-use crate::models::enums::CheerPracticeAttendanceType;
+use crate::{models::enums::CheerPracticeAttendanceType, prelude::*};
 
-#[derive(Clone, Debug, Deserialize, FromRow, GetById)]
+#[derive(Clone, Debug, Deserialize, Serialize, FromRow, GetById)]
 #[from_query(query = "
     SELECT
         id, created_at, practice_period_id, student_id, checker_id, presence, presence_at_end,
@@ -22,4 +23,17 @@ pub struct DbCheerPracticeAttendance {
     pub presence: CheerPracticeAttendanceType,
     pub presence_at_end: Option<CheerPracticeAttendanceType>,
     pub absence_reason: Option<String>,
+}
+
+impl DbCheerPracticeAttendance {
+    pub async fn get_by_student_id(conn: &mut PgConnection, student_id: Uuid) -> Result<Vec<Uuid>> {
+        let res = query!(
+            "SELECT id FROM cheer_practice_attendances WHERE student_id = $1",
+            student_id
+        )
+        .fetch_all(conn)
+        .await?;
+
+        Ok(res.into_iter().map(|r| r.id).collect())
+    }
 }
