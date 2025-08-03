f68c815503828553eac2283bf7b66a91512404b6
owo93 <iztyle30@gmail.com>
2025-08-03 21:49:02 +0700

    perf: restrict querying cheer practice period descendants to only `IdOnly` and `Compact`
    only allow `descendant_fetch_level` `IdOnly` and `Compact` for routes:
    - `query_practice_periods`
    - `query_practice_period_details`
    
    for performance reasons, which is (_planned?_) to be fixed later

--- mysk-data-api/src/routes/v1/attendance/cheer/periods/query_practice_period.rs
+++ mysk-data-api/src/routes/v1/attendance/cheer/periods/query_practice_period.rs
@@ -5,7 +5,7 @@ use crate::{
 use actix_web::{HttpResponse, Responder, get, web::Data};
 use mysk_lib::{
     common::{
-        requests::{EmptyRequestData, RequestType},
+        requests::{EmptyRequestData, FetchLevel, RequestType},
         response::{MetadataType, ResponseType},
     },
     models::cheer_practice_period::{
@@ -34,6 +34,18 @@ pub async fn query_practice_period(
     let mut conn = data.db.acquire().await?;
     let authorizer =
         Authorizer::new(&mut conn, &user, "/attendance/cheer/periods".to_string()).await?;
+
+    // TODO: Remove this later
+    if !matches!(
+        descendant_fetch_level,
+        FetchLevel::IdOnly | FetchLevel::Compact
+    ) {
+        return Err(Error::InvalidRequest(
+            "Requested descendant_fetch_level is too resource intensive. Use `IdOnly` or `Compact` instead."
+                .to_string(),
+            "/attendance/cheer/periods".to_string()
+        ));
+    }
 
     let (practice_periods, pagination) = CheerPracticePeriod::query(
         pool,
