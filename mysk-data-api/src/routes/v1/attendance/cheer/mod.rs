47922b56fc8c5572fd87406ee749406eee07c58d
owo93 <iztyle30@gmail.com>
2025-10-09 16:29:43 +0700

    fix: deprecate temporary route query_classroom_cheer_practice_attendance

--- mysk-data-api/src/routes/v1/attendance/cheer/mod.rs
+++ mysk-data-api/src/routes/v1/attendance/cheer/mod.rs
@@ -1,11 +1,7 @@
 use actix_web::web::{ServiceConfig, scope};
 
 pub mod periods;
-pub mod query_classroom_cheer_practice_attendance;
 
 pub fn config(cfg: &mut ServiceConfig) {
     cfg.service(scope("/periods").configure(periods::config))
-        .service(
-            query_classroom_cheer_practice_attendance::query_classroom_cheer_practice_attendance,
-        );
 }
