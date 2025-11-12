f6dd48e15e2d700a48701aa85d7afab4a22b8ca3
owo93 <iztyle30@gmail.com>
2025-11-12 15:58:44 +0700

    fix: use date in in-jaturamitr-period route instead of Uuid :skull:

--- mysk-data-api/src/routes/v1/attendance/cheer/in_jaturamitr_period.rs
+++ mysk-data-api/src/routes/v1/attendance/cheer/in_jaturamitr_period.rs
@@ -1,24 +1,32 @@
+use std::collections::HashSet;
+
 use crate::{AppState, extractors::api_key::ApiKeyHeader};
 use actix_web::{
     HttpResponse, Responder, get,
     web::{Data, Path},
 };
-use mysk_lib::{
-    common::response::ResponseType, models::cheer_practice_period::db::DbCheerPracticePeriod,
-    prelude::*,
-};
-use uuid::Uuid;
+use chrono::NaiveDate;
+use mysk_lib::{common::response::ResponseType, prelude::*};
 
-#[get("/in-jaturamitr-period/{id}")]
+#[get("/in-jaturamitr-period/{date}")]
 pub async fn in_jaturamitr_period(
     _data: Data<AppState>,
     _: ApiKeyHeader,
-    practice_period_id: Path<Uuid>,
+    date: Path<NaiveDate>,
 ) -> Result<impl Responder> {
-    let is_in_jaturamitr_period =
-        DbCheerPracticePeriod::in_jaturamitr_period(practice_period_id.into_inner());
+    let jaturamitr_dates = HashSet::from([
+        NaiveDate::from_ymd_opt(2025, 11, 13).unwrap(),
+        NaiveDate::from_ymd_opt(2025, 11, 15).unwrap(),
+        NaiveDate::from_ymd_opt(2025, 11, 22).unwrap(),
+    ]);
 
-    let response = ResponseType::new(is_in_jaturamitr_period, None);
+    let is_jaturamitr_day = if jaturamitr_dates.contains(&date.into_inner()) {
+        true
+    } else {
+        false
+    };
+
+    let response = ResponseType::new(is_jaturamitr_day, None);
 
     Ok(HttpResponse::Ok().json(response))
 }
