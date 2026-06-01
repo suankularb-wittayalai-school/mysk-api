a7c35fb9111c8a0fc17382ef8bce7eb818934d7c
owo93 <iztyle30@gmail.com>
2026-06-01 21:13:17 +0700

    ❄️: add pre-commit hooks to lint & fmt
    - added clippy and rustfmt git-hooks
    - use rust packages directly from flake-utils' `inputs'`

--- flake.nix
+++ flake.nix
@@ -1,58 +1,82 @@
 {
   description = "MySK API Rust Dev Environment";
+
   inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
-    rust-overlay = {
-      url = "github:oxalica/rust-overlay";
+    fenix = {
+      url = "github:nix-community/fenix";
       inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    git-hooks = {
+      url = "github:cachix/git-hooks.nix";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+      };
     };
   };
 
   outputs =
-    {
-      nixpkgs,
-      flake-utils,
-      rust-overlay,
-      ...
-    }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        overlays = [ (import rust-overlay) ];
-        pkgs = import nixpkgs {
-          inherit system overlays;
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        inputs.git-hooks.flakeModule
+      ];
+
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
+
+      perSystem =
+        {
+          config,
+          inputs',
+          pkgs,
+          ...
+        }:
+        let
+          rustToolchain = inputs'.fenix.packages.stable;
+        in
+        {
+          devShells.default = pkgs.mkShell {
+            name = "mysk-rs";
+
+            inputsFrom = [ config.pre-commit.devShell ];
+
+            packages = with pkgs; [
+              cargo-watch
+              sqlx-cli
+              pkg-config
+
+              (rustToolchain.withComponents [
+                "cargo"
+                "clippy"
+                "rust-src"
+                "rustc"
+                "rustfmt"
+                "rust-analyzer"
+              ])
+            ];
+
+            shellHook = ''
+              echo -e "Welcome to the \e[0;35mMy\e[0;36mSK\e[0m API Rust development environment!"
+              export CARGO_BUILD_TARGET="${pkgs.stdenv.hostPlatform.rust.rustcTarget}"
+            '';
+          };
+
+          pre-commit = {
+            check.enable = true;
+            settings.hooks = {
+              clippy = {
+                enable = true;
+                settings.extraArgs = "--no-deps";
+              };
+              rustfmt.enable = true;
+            };
+          };
         };
-
-        rustTarget = pkgs.stdenv.hostPlatform.rust.rustcTarget;
-        rustToolchain = pkgs.rust-bin.stable.latest.default.override {
-          extensions = [
-            "rust-src"
-            "rust-analyzer"
-          ];
-          targets = [ rustTarget ];
-        };
-      in
-      {
-        devShells.default = pkgs.mkShell {
-          name = "mysk-rs";
-
-          strictDeps = true;
-          packages = with pkgs; [
-            cargo-watch
-            sqlx-cli
-          ];
-
-          nativeBuildInputs = with pkgs; [
-            pkg-config
-            rustToolchain
-          ];
-
-          shellHook = ''
-            echo -e "Welcome to the \e[0;35mMy\e[0;36mSK\e[0m API Rust development environment!"
-            export CARGO_BUILD_TARGET="${rustTarget}"
-          '';
-        };
-      }
-    );
+    };
 }
